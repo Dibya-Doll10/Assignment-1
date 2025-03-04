6265f86d067bcdb2f8b4822df44749733f333581
Dibya-Doll10 <dibyananda.das2345@gmail.com>
2025-03-04 09:07:53 -0800

    Add files via upload

---
+++ First Assignment Task1.py
@@ -0,0 +1,14 @@
+a = input("Enter the first num:")
+b = input("Enter the second num:")
+
+sum = int(a) + int(b)
+print("The sum of the numbers is ", sum)
+
+sub = int(a)-int(b)
+print("The subtraction of the numbers is ",sub)
+
+mult = int(a) * int(b)
+print("The product of two numbers: ",mult)
+
+div = float(a)/float(b)
+print("The division is: ", div)
---
+++ First Assignment Task2.py
@@ -0,0 +1,3 @@
+a = input("Enter the first name: ")
+b = input("Enter the second name: ")
+print("Hello! Good morning "+a +" "+b+"! Welcome to the world of coding!" )
